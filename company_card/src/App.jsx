2702b86b4aac943a4e542c372fd70e74e8da87fd
khusarika <81645165+khusarika@users.noreply.github.com>
2026-06-22 17:40:29 +0530

    company-card done

---
+++ company_card/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <div>
+      <h1>Hello</h1>
+    </div>
+    <h1>Fragments</h1>
+    </>
+    
+  )
+}
+
+export default App
+
